9a5bce5e106a6e12707648c3e407d4bf395233de
ida <idasusannajohansson@gmail.com>
2022-11-11 15:38:27 +0100

    Added Sql queries to methods

--- CRUD-Person/Models/ReadPerson.cs
+++ CRUD-Person/Models/ReadPerson.cs
@@ -1,61 +1,152 @@
 ﻿using System;
 using System.Linq;
 using CRUD_Person.Interfaces;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 
 namespace CRUD_Person.Models
 {
     public class ReadPerson : IReadPerson
     {
+        // Using SQL queries
         public void Read()
         {
             try
             {
-            var dbContext = new PersonDbContext();
+                Console.WriteLine("Enter an Id to get Person details:");
+                int id = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                using (var dbContext = new PersonDbContext())
+                {
+                    Console.WriteLine("Person details: ");
+                    Console.WriteLine();
+                    var queryString = dbContext.Person.FromSqlRaw($"SELECT Id,FirstName, LastName, Email, PhoneNr FROM Person where Id = {id}").ToList();
 
-            Console.WriteLine("Enter an Id to get Person details:");
-            int id = Convert.ToInt32(Console.ReadLine());  
-
-            var person = dbContext.Person.Find(id);
-
-            Console.WriteLine($"Id: {person.Id}");
-            Console.WriteLine($"First Name: {person.FirstName}");
-            Console.WriteLine($"Last Name: {person.LastName}");
-            Console.WriteLine($"Email: {person.Email}");
-            Console.WriteLine($"Phone Number: {person.PhoneNr}");
+                    foreach (var person in queryString)
+                    {
+                        Console.WriteLine($"Id: {person.Id}");
+                        Console.WriteLine($"First Name: {person.FirstName}");
+                        Console.WriteLine($"Last Name: {person.LastName}");
+                        Console.WriteLine($"Email: {person.Email}");
+                        Console.WriteLine($"Phone Number: {person.PhoneNr}");
+                    }
+                }
             }
             catch (Exception e)
             {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine(e.Message);
                 Console.WriteLine("Something went wrong when Reading Person, please try again");
+                Console.ResetColor();
             }
+            Console.WriteLine();
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Press a key to return to Menu");
+            Console.ReadKey();
+            Console.Clear();
         }
 
         public void GetAllPersons()
         {
             try
             {
-            var dbContext = new PersonDbContext();
-            var listAllPersons = dbContext.Person.ToList();
-            foreach (var person in listAllPersons)
-            {
-                Console.WriteLine($"Id: {person.Id}");
-                Console.WriteLine($"First Name: {person.FirstName}");
-                Console.WriteLine($"Last Name: {person.LastName}");
-                Console.WriteLine($"Email: {person.Email}");
-                Console.WriteLine($"Phone Number: {person.PhoneNr}");
-                Console.WriteLine();
-                Console.WriteLine("********************************");
-                Console.WriteLine();
-            }
+                using (var dbContext = new PersonDbContext())
+                {
+                    var queryString = dbContext.Person.FromSqlRaw($"SELECT Id,FirstName, LastName, Email, PhoneNr FROM Person").ToList();
+                    Console.WriteLine($"Numer of persons registered in PersonDB: {queryString.Count()}");
+                    Console.WriteLine("------------------------------");
+                    Console.WriteLine("Person details: ");
+                    Console.WriteLine();
 
+                    foreach (var person in queryString)
+                    {
+                        Console.WriteLine($"Id: {person.Id}");
+                        Console.WriteLine($"First Name: {person.FirstName}");
+                        Console.WriteLine($"Last Name: {person.LastName}");
+                        Console.WriteLine($"Email: {person.Email}");
+                        Console.WriteLine($"Phone Number: {person.PhoneNr}");
+                        Console.WriteLine();
+                        Console.WriteLine("********************************");
+                        Console.WriteLine();
+                    }
+                }
             }
             catch (Exception e)
             {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine(e.Message);
                 Console.WriteLine("Something went wrong when Reading Persons, please try again");
+                Console.ResetColor();
             }
+            Console.WriteLine();
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Press a key to return to Menu");
+            Console.ReadKey();
+            Console.Clear();
         }
-
     }
+
+    //public void Read()
+    //{
+    //    try
+    //    {
+    //        var dbContext = new PersonDbContext();
+
+    //        Console.WriteLine("Enter an Id to get Person details:");
+    //        int id = Convert.ToInt32(Console.ReadLine());
+
+    //        var person = dbContext.Person.Find(id);
+
+    //        Console.WriteLine($"Id: {person.Id}");
+    //        Console.WriteLine($"First Name: {person.FirstName}");
+    //        Console.WriteLine($"Last Name: {person.LastName}");
+    //        Console.WriteLine($"Email: {person.Email}");
+    //        Console.WriteLine($"Phone Number: {person.PhoneNr}");
+    //    }
+    //    catch (Exception e)
+    //    {
+    //        Console.WriteLine(e.Message);
+    //        Console.WriteLine("Something went wrong when Reading Person, please try again");
+    //    }
+    //    Console.WriteLine();
+    //    Console.WriteLine("------------------------------");
+    //    Console.WriteLine("Press a key to return to Menu");
+    //    Console.ReadKey();
+    //    Console.Clear();
+    //}
+
+    //public void GetAllPersons()
+    //{
+    //    try
+    //    {
+    //    var dbContext = new PersonDbContext();
+    //    var listAllPersons = dbContext.Person.ToList();
+    //        foreach (var person in listAllPersons)
+    //        {
+    //            Console.WriteLine($"Id: {person.Id}");
+    //            Console.WriteLine($"First Name: {person.FirstName}");
+    //            Console.WriteLine($"Last Name: {person.LastName}");
+    //            Console.WriteLine($"Email: {person.Email}");
+    //            Console.WriteLine($"Phone Number: {person.PhoneNr}");
+    //            Console.WriteLine();
+    //            Console.WriteLine("********************************");
+    //            Console.WriteLine();
+    //        }
+    //    }
+    //    catch (Exception e)
+    //    {
+    //        Console.ForegroundColor = ConsoleColor.DarkRed;
+    //        Console.WriteLine(e.Message);
+    //        Console.WriteLine("Something went wrong when Reading Persons, please try again");
+    //        Console.ResetColor();
+    //    }
+    //    Console.WriteLine();
+    //    Console.WriteLine("------------------------------");
+    //    Console.WriteLine("Press a key to return to Menu");
+    //    Console.ReadKey();
+    //    Console.Clear();
+    //}
+
 }
+
 
--- CRUD-Person/Models/UpdatePerson.cs
+++ CRUD-Person/Models/UpdatePerson.cs
@@ -5,30 +5,36 @@ namespace CRUD_Person.Models
 {
     public class UpdatePerson : IUpdatePerson
     {
+
         public void Update()
         {
             try
             {
-            var dbContext = new PersonDbContext();
-            Console.WriteLine("Enter an Id to Update Email and Phonenumber:");
-            int id = Convert.ToInt32(Console.ReadLine()); 
-            var updPerson = dbContext.Person.Find(id);
+                var dbContext = new PersonDbContext();
+                Console.WriteLine("Enter an Id to Update Email and Phonenumber:");
+                int id = Convert.ToInt32(Console.ReadLine());
+                var updPerson = dbContext.Person.Find(id);
 
-            Console.WriteLine("Email: ");
-            string email = Console.ReadLine(); 
-            updPerson.Email = email;
-            Console.WriteLine("PhoneNumber: ");
-            int phoneNr = Convert.ToInt32(Console.ReadLine());
-            updPerson.PhoneNr = phoneNr;
+                Console.WriteLine("Email: ");
+                string email = Console.ReadLine();
+                updPerson.Email = email;
+                Console.WriteLine("PhoneNumber: ");
+                int phoneNr = Convert.ToInt32(Console.ReadLine());
+                updPerson.PhoneNr = phoneNr;
 
-            dbContext.SaveChanges();
-            Console.WriteLine("Person is Updated!");
+                dbContext.SaveChanges();
+                Console.WriteLine("Person is Updated!");
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
                 Console.WriteLine($"Something whent wrong when updating Person, please try again.");
             }
+            Console.WriteLine();
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Press a key to return to Menu");
+            Console.ReadKey();
+            Console.Clear();
         }
     }
 }
